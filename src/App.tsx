b82113f2b58e6545e48576ef5540604c50881953
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-04-30 17:05:41 -0300

    add: home page and router assigns

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import './App.css'
 import Header from './components/Header'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { Home } from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }
+]);
 
 function App() {
   return (
@@ -11,7 +20,7 @@ function App() {
       <Header />
     </header>
     <main>
-      
+      <RouterProvider router={router} />
     </main>
     <footer>
 
