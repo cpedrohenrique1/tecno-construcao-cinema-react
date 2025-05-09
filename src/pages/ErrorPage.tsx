8a67cbf2921294fd893ee312f3f91a07dd934612
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-05-09 16:42:51 -0300

    add: routes errorpage and home

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,11 @@
+export function ErrorPage() {
+    return (
+        <>
+            <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh"}}>
+                <h1>404</h1>
+                <h2>Página não encontrada</h2>
+                <p>Desculpe, a página que você está procurando não existe.</p>
+            </div>
+        </>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,23 @@
+import { Card } from "../components/Card"
+import { Filme } from "../interfaces/Filme.interface"
+
 export const Home = () => {
+  const filme: Filme = {
+    titulo: "a era do gelo",
+    classificacao: 13,
+    descricao: "Um esquilo vai comer uma noz e quebra o gelo",
+    duracao: 130,
+    genero: "Adulto",
+    dataEstreia: new Date(),
+    id: 1
+  };
   return (
-    <div>Home</div>
+    <>
+      <div className="container ">
+        <div className="row">
+          <Card filme={filme}/>
+        </div>
+      </div>
+    </>
   )
 }
