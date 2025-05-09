64b43d426698ede87d6fcf838fbb57e3bea47c79
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-05-09 16:40:45 -0300

    add: Created filme interface

---
+++ src/interfaces/Filme.interface.ts
@@ -0,0 +1,9 @@
+export interface Filme {
+    id: number,
+    titulo: string,
+    descricao: string,
+    classificacao: number,
+    genero: string,
+    duracao: number,
+    dataEstreia: Date,
+}
