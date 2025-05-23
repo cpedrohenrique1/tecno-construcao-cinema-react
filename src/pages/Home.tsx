8ba9050e5b9f0582c8a702050b2211441c4197ed
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-05-23 16:50:45 -0300

    update: Added more cards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,40 @@
+import Filme from "../classes/Filme";
 import { Card } from "../components/Card"
-import { Filme } from "../interfaces/Filme.interface"
+import { FilmeInterface } from "../interfaces/Filme.interface"
 
 export const Home = () => {
-  const filme: Filme = {
-    titulo: "a era do gelo",
-    classificacao: "13",
-    descricao: "Um esquilo vai comer uma noz e quebra o gelo",
-    duracao: 130,
-    genero: "Adulto",
-    dataEstreia: new Date(),
-    id: 1
-  };
+  const filme: FilmeInterface = new Filme(
+    1,
+    "a era do gelo",
+    "Um esquilo vai comer uma noz e quebra o gelo",
+    "13",
+    "Adulto",
+    130,
+    new Date()
+  );
   const nomeBotao: string = "Comprar ingressos";
   return (
     <>
       <div className="container ">
-        <br/>
-          <div className="row">
-            <Card filme={filme} nomeBotao={nomeBotao}/>
-            <Card filme={filme} nomeBotao={nomeBotao}/>
-          </div>
+        <br />
+        <div className="row">
+          <Card filme={filme} nomeBotao={nomeBotao} />
+          <Card filme={filme} nomeBotao={nomeBotao} />
+          <Card filme={filme} nomeBotao={nomeBotao} />
+          <Card filme={filme} nomeBotao={nomeBotao} />
+          <Card filme={filme} nomeBotao={nomeBotao} />
+          <Card filme={filme} nomeBotao={nomeBotao} />
+          <Card filme={filme} nomeBotao={nomeBotao} />
+          <Card filme={filme} nomeBotao={nomeBotao} />
+          <Card filme={filme} nomeBotao={nomeBotao} />
+          <Card filme={filme} nomeBotao={nomeBotao} />
+          <Card filme={filme} nomeBotao={nomeBotao} />
+          <Card filme={filme} nomeBotao={nomeBotao} />
+          <Card filme={filme} nomeBotao={nomeBotao} />
+          <Card filme={filme} nomeBotao={nomeBotao} />
+          <Card filme={filme} nomeBotao={nomeBotao} />
+          <Card filme={filme} nomeBotao={nomeBotao} />
+        </div>
       </div>
     </>
   )
