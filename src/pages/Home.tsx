0237d7036ec63bc8954b51b35431210db1d86fad
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-05-26 16:12:01 -0300

    refactor: Homepage now get filmes from localStorage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,33 @@
 import { NavigateFunction, useNavigate } from "react-router-dom";
-import Filme from "../classes/Filme";
 import { Card } from "../components/Card"
 import { FilmeInterface } from "../interfaces/Filme.interface"
+import FilmeService from "../services/filmeService";
 
 export const Home = () => {
-  const filme: FilmeInterface = new Filme(
-    1,
-    "a era do gelo",
-    "Um esquilo vai comer uma noz e quebra o gelo",
-    "13",
-    "Adulto",
-    130,
-    new Date()
-  );
+  const filmeService = new FilmeService();
+  const filmes: FilmeInterface[] = filmeService.getFilmesFromLocalStorage();
   const nomeBotao: string = "Comprar ingressos";
   const navigate: NavigateFunction = useNavigate();
 
+  const cards = [];
+  for (const filme of filmes) {
+    cards.push(
+      <Card filme={filme} nomeBotao={nomeBotao} acaoBotao={() => {
+            navigate(`/ingressos/${filme.getId()}`);
+          }} />
+    )
+  }
+  if (cards.length === 0) {
+    cards.push(
+      <h2>Não há filmes salvos</h2>
+    )
+  }
   return (
     <>
       <div className="container ">
         <br />
         <div className="row">
-          <Card filme={filme} nomeBotao={nomeBotao} acaoBotao={() => {
-            navigate(`/ingressos/${filme.getId()}`);
-          }} />
+          {cards}
         </div>
       </div>
     </>
