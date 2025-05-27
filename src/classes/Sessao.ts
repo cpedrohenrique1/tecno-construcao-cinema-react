689ae0145365fbc60e55838ea184df045ba66711
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-05-26 22:26:49 -0300

    add: Sessoes class & Interface

---
+++ src/classes/Sessao.ts
@@ -0,0 +1,77 @@
+import { FilmeInterface } from "../interfaces/Filme.interface";
+import SalaInterface from "../interfaces/Sala.interface";
+import SessaoInterface from "../interfaces/Sessoes.interface";
+import Filme from "./Filme";
+import Sala from "./Sala";
+
+export default class Sessao implements SessaoInterface {
+    id: number;
+    filme: FilmeInterface;
+    sala: SalaInterface;
+    dataHora: Date;
+    preco: number;
+    idioma: string;
+    formato: string;
+
+    constructor(
+        id: number = 0,
+        filme: FilmeInterface = new Filme(),
+        sala: SalaInterface = new Sala(),
+        dataHora: Date = new Date(),
+        preco: number = 0,
+        idioma: string = "",
+        formato: string = ""
+    ) {
+        this.id = id;
+        this.filme = filme;
+        this.sala = sala;
+        this.dataHora = dataHora;
+        this.preco = preco;
+        this.idioma = idioma;
+        this.formato = formato;
+    }
+
+    getId(): number {
+        return this.id;
+    }
+    setId(id: number): void {
+        this.id = id;
+    }
+    getFilme(): FilmeInterface {
+        return this.filme;
+    }
+    setFilme(filme: FilmeInterface): void {
+        this.filme = filme;
+    }
+    getSala(): SalaInterface {
+        return this.sala;
+    }
+    setSala(sala: SalaInterface): void {
+        this.sala = sala;
+    }
+    getDataHora(): Date {
+        return this.dataHora;
+    }
+    setDataHora(dataHora: Date): void {
+        this.dataHora = dataHora;
+    }
+    getPreco(): number {
+        return this.preco;
+    }
+    setPreco(preco: number): void {
+        this.preco = preco;
+    }
+    getIdioma(): string {
+        return this.idioma;
+    }
+    setIdioma(idioma: string): void {
+        this.idioma = idioma;
+    }
+    getFormato(): string {
+        return this.formato;
+    }
+    setFormato(formato: string): void {
+        this.formato = formato;
+    }
+
+}
---
+++ src/interfaces/Sessoes.interface.ts
@@ -0,0 +1,27 @@
+import { FilmeInterface } from "./Filme.interface";
+import SalaInterface from "./Sala.interface";
+
+export default interface SessaoInterface{
+    id: number,
+    filme: FilmeInterface,
+    sala: SalaInterface,
+    dataHora: Date,
+    preco: number,
+    idioma: string,
+    formato: string,
+    
+    getId(): number;
+    setId(id: number): void;
+    getFilme(): FilmeInterface;
+    setFilme(filme: FilmeInterface): void;
+    getSala(): SalaInterface;
+    setSala(sala: SalaInterface): void;
+    getDataHora(): Date;
+    setDataHora(dataHora: Date): void;
+    getPreco(): number;
+    setPreco(preco: number): void;
+    getIdioma(): string;
+    setIdioma(idioma: string): void;
+    getFormato(): string;
+    setFormato(formato: string): void;
+}
