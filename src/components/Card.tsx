9d83aec17590c2030fe9d1fbdc6c1b99599afd5c
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-06-03 17:08:47 -0300

    fix: Card recebe sessao ao inves de filmes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,20 @@
 import { FilmeInterface } from "../interfaces/Filme.interface";
+import SessaoInterface from "../interfaces/Sessoes.interface";
 import Button from "./Button";
 
-export function Card({filme, nomeBotao = "Comprar ingressos", acaoBotao}: {filme: FilmeInterface, nomeBotao: string, acaoBotao: () => void}) {
+export function Card({sessao, nomeBotao = "Comprar ingressos", acaoBotao}: {sessao: SessaoInterface, nomeBotao: string, acaoBotao: () => void}) {
     return (
         <>
             <div className="col-12 col-sm-6 col-lg-4" style={{ marginBottom: "20px" }}>
                 <div className="card">
                     <div className="card-body">
-                        <h5 className="card-title">{filme.titulo}</h5>
-                        <p className="cart-text">{filme.descricao}</p>
-                        <p className="card-text"><strong>Gênero: </strong>{filme.genero}</p>
-                        <p className="card-text"><strong>Classificação: </strong>{filme.classificacao}</p>
-                        <p className="card-text"><strong>Duração: </strong>{filme.duracao}</p>
-                        <p className="card-text"><strong>Data de Estreia: </strong>{filme.dataEstreia.toLocaleDateString()}</p>
-                        <Button id={filme.id.toString()} nome={nomeBotao} classes={"btn-primary"} action={acaoBotao} />
+                        <h5 className="card-title">Nome do filme: {sessao.getFilme().getTitulo()}</h5>
+                        <p className="cart-text">Nome sala: {sessao.getSala().getNome()}</p>
+                        <p className="card-text"><strong>Tipo sala: </strong>{sessao.getFormato()}</p>
+                        <p className="card-text"><strong>Classificação: </strong>{sessao.getFilme().getClassificacao()}</p>
+                        <p className="card-text"><strong>Duração: </strong>{sessao.getFilme().getDuracao()}</p>
+                        <p className="card-text"><strong>Data de Estreia: </strong>{sessao.getDataHora().toLocaleDateString()}</p>
+                        <Button id={sessao.getId().toString()} nome={nomeBotao} classes={"btn-primary"} action={acaoBotao} />
                     </div>
                 </div>
             </div>
