9610252e48dc54bd5404e2d4cfcbd536a33b296f
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-06-10 20:27:12 +0000

    feat: filmeService get from API

--- src/services/filmeService.ts
+++ src/services/filmeService.ts
@@ -1,12 +1,13 @@
 import Filme from "../classes/Filme"
 import { FilmeInterface } from "../interfaces/Filme.interface";
+import { api } from "./apiService";
 
 export default class FilmeService {
 
-    getFilmesFromLocalStorage(): FilmeInterface[] {
-        const filmesJson = localStorage.getItem('filmes');
-        if (filmesJson) {
-            const filmesArray: Filme[] = JSON.parse(filmesJson);
+    async getFilmesFromApi() {
+        const obj = await api.get('filmes');
+        if (obj) {
+            const filmesArray: Filme[] = obj.data;
             return filmesArray.map(f => new Filme(f.id, f.titulo, f.descricao, f.classificacao, f.genero, f.duracao, new Date(f.dataEstreia)));
         }
         return [];
